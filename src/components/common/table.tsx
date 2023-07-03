02c552ffeadf9d9410f71781edaafee1236b55a6
ahonn <ahonn95@outlook.com>
2023-07-03 11:03:00 +0800

    fix: fix table loading react error

--- src/components/common/table.tsx
+++ src/components/common/table.tsx
@@ -82,122 +82,128 @@ export default function Table<Data extends Record<string, any>>(
   });
 
   return (
-    <TableContainer
-      borderWidth="1px"
-      borderColor="gray.700"
-      borderRadius="16px"
-    >
-      <ChakraTable variant="simple">
-        <Thead
-          backgroundColor="secondary"
-          borderBottom="1px"
-          borderColor="gray.100"
-        >
-          {table.getHeaderGroups().map((headerGroup) => (
-            <Tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                const meta: any = header.column.columnDef.meta;
-                return (
-                  <Th
-                    key={header.id}
-                    isNumeric={meta?.isNumeric}
-                    textTransform="capitalize"
-                    padding="16px"
-                    color="black"
-                    fontFamily="montserrat"
-                    fontSize="sm"
-                  >
-                    {flexRender(
-                      header.column.columnDef.header,
-                      header.getContext(),
-                    )}
-                  </Th>
-                );
-              })}
-            </Tr>
-          ))}
-        </Thead>
-        <Tbody position="relative" height={isLoading ? '200px' : 'auto'}>
-          {isLoading && (
-            <Flex
-              position="absolute"
-              width="100%"
-              height="100%"
-              backgroundColor="white"
-              opacity="70%"
-              justifyContent="center"
-              alignItems="center"
-              cursor="wait"
-            >
-              <Spinner color="brand" thickness="4px" size="xl" />
-            </Flex>
-          )}
-
-          {table.getRowModel().rows.map((row) => (
-            <Fragment key={row.id}>
-              <Tr>
-                {row.getVisibleCells().map((cell, index) => {
-                  const meta: any = cell.column.columnDef.meta;
+    <Box position="relative">
+      <TableContainer
+        borderWidth="1px"
+        borderColor="gray.700"
+        borderRadius="16px"
+      >
+        <ChakraTable variant="simple">
+          <Thead
+            backgroundColor="secondary"
+            borderBottom="1px"
+            borderColor="gray.100"
+          >
+            {table.getHeaderGroups().map((headerGroup) => (
+              <Tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  const meta: any = header.column.columnDef.meta;
                   return (
-                    <Td
-                      key={cell.id}
+                    <Th
+                      key={header.id}
                       isNumeric={meta?.isNumeric}
+                      textTransform="capitalize"
                       padding="16px"
+                      color="black"
                       fontFamily="montserrat"
-                      fontSize="14px"
-                      backgroundColor="white"
+                      fontSize="sm"
                     >
-                      <Flex alignItems="center" gap={1}>
-                        {row.getCanExpand() &&
-                          index === 0 &&
-                          props.expandable && (
-                            <Icon
-                              as={
-                                row.getIsExpanded()
-                                  ? MdExpandMore
-                                  : MdChevronRight
-                              }
-                              width="24px"
-                              height="24px"
-                              cursor="pointer"
-                              onClick={row.getToggleExpandedHandler()}
-                            />
-                          )}
-                        {flexRender(
-                          cell.column.columnDef.cell,
-                          cell.getContext(),
-                        )}
-                      </Flex>
-                    </Td>
+                      {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext(),
+                      )}
+                    </Th>
                   );
                 })}
               </Tr>
-              <AnimatePresence initial={false}>
-                {row.getIsExpanded() && props.expandable?.expandedRowRender && (
-                  <Tr>
-                    <Td colSpan={row.getVisibleCells().length} padding={0}>
-                      <Box
-                        paddingLeft={5}
-                        backgroundColor="yellow.100"
-                        as={motion.div}
-                        initial="collapsed"
-                        animate="open"
-                        exit="collapsed"
-                        variants={{
-                          open: { opacity: 1, height: 'auto' },
-                          collapsed: { opacity: 0, height: 0 },
-                        }}
+            ))}
+          </Thead>
+          <Tbody position="relative" height={isLoading ? '200px' : 'auto'}>
+            {table.getRowModel().rows.map((row) => (
+              <Fragment key={row.id}>
+                <Tr>
+                  {row.getVisibleCells().map((cell, index) => {
+                    const meta: any = cell.column.columnDef.meta;
+                    return (
+                      <Td
+                        key={cell.id}
+                        isNumeric={meta?.isNumeric}
+                        padding="16px"
+                        fontFamily="montserrat"
+                        fontSize="14px"
+                        backgroundColor="white"
                       >
-                        {props.expandable.expandedRowRender(row.original)}
-                      </Box>
-                    </Td>
-                  </Tr>
-                )}
-              </AnimatePresence>
-            </Fragment>
-          ))}
-        </Tbody>
-      </ChakraTable>
-    </TableContainer>
+                        <Flex alignItems="center" gap={1}>
+                          {row.getCanExpand() &&
+                            index === 0 &&
+                            props.expandable && (
+                              <Icon
+                                as={
+                                  row.getIsExpanded()
+                                    ? MdExpandMore
+                                    : MdChevronRight
+                                }
+                                width="24px"
+                                height="24px"
+                                cursor="pointer"
+                                onClick={row.getToggleExpandedHandler()}
+                              />
+                            )}
+                          {flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext(),
+                          )}
+                        </Flex>
+                      </Td>
+                    );
+                  })}
+                </Tr>
+                <AnimatePresence initial={false}>
+                  {row.getIsExpanded() &&
+                    props.expandable?.expandedRowRender && (
+                      <Tr>
+                        <Td colSpan={row.getVisibleCells().length} padding={0}>
+                          <Box
+                            paddingLeft={5}
+                            backgroundColor="yellow.100"
+                            as={motion.div}
+                            initial="collapsed"
+                            animate="open"
+                            exit="collapsed"
+                            variants={{
+                              open: { opacity: 1, height: 'auto' },
+                              collapsed: { opacity: 0, height: 0 },
+                            }}
+                          >
+                            {props.expandable.expandedRowRender(row.original)}
+                          </Box>
+                        </Td>
+                      </Tr>
+                    )}
+                </AnimatePresence>
+              </Fragment>
+            ))}
+          </Tbody>
+        </ChakraTable>
+      </TableContainer>
+
+      {isLoading && (
+        <Flex
+          position="absolute"
+          top="0px"
+          left="0px"
+          width="100%"
+          height="100%"
+          borderRadius="16px"
+          backgroundColor="white"
+          opacity="30%"
+          justifyContent="center"
+          alignItems="center"
+          cursor="wait"
+        >
+          <Spinner color="brand" thickness="4px" size="xl" />
+        </Flex>
+      )}
+    </Box>
   );
 }
