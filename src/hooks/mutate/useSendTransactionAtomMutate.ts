a120c6a01d3ee1e1044ddfc6b290c4853bb4b7e3
ahonn <ahonn95@outlook.com>
2023-07-14 17:02:43 +0800

    feat: add signMessage and fill witnesses

--- src/hooks/mutate/useSendTransactionAtomMutate.ts
+++ src/hooks/mutate/useSendTransactionAtomMutate.ts
@@ -1,19 +1,14 @@
-import { CellDep, config } from '@ckb-lumos/lumos';
-// import { blockchain } from '@ckb-lumos/base';
-// import { bytes } from '@ckb-lumos/codec';
+import { CellDep, RawTransaction, Transaction, commons, config } from '@ckb-lumos/lumos';
+import { blockchain } from '@ckb-lumos/base';
+import { bytes } from '@ckb-lumos/codec';
 import { WritableAtom, useSetAtom } from 'jotai';
 import { useCallback, useState } from 'react';
-// import { signMessage } from '@wagmi/core';
+import transaction from './transaction.mock.json';
+import { signMessage } from '@wagmi/core';
+import { omit } from 'lodash-es';
+import camelcaseKeys from 'camelcase-keys';
 
 type ScriptName = keyof (typeof config.predefined.AGGRON4)['SCRIPTS'];
-
-// const OMNILOCK_SIGNATURE_PLACEHOLDER = bytes.hexify(
-//   new Uint8Array(
-//     commons.omnilock.OmnilockWitnessLock.pack({
-//       signature: new Uint8Array(65).buffer,
-//     }).byteLength,
-//   ),
-// );
 
 export function getScriptCellDep(name: ScriptName): CellDep {
   const script = config.predefined.AGGRON4['SCRIPTS'][name];
@@ -37,55 +32,33 @@ export function useSendTransactionAtomMutate<Value, Args extends any[], Result>(
       setIsLoading(true);
       try {
         const result: any = await mutate(...args);
+        const { hash } = transaction;
+        let signature = await signMessage({ message: hash });
 
-        // TODO: create transaction and send it to the spark
+        let v = Number.parseInt(signature.slice(-2), 16);
+        if (v >= 27) v -= 27;
+        signature = ('0x' + signature.slice(2, -2) + v.toString(16).padStart(2, '0')) as `0x${string}`;
 
-        // let tx = helpers
-        //   .TransactionSkeleton({})
-        //   .update('inputs', (inputs) => inputs.push(...result.inputs))
-        //   .update('outputs', (outputs) => outputs.push(...result.outputs));
+        const tx = omit(camelcaseKeys(transaction, { deep: true }), 'hash');
 
-        // const inputs = tx.get('inputs');
-        // tx = tx.update('cellDeps', (cellDeps) =>
-        //   cellDeps.push(
-        //     getScriptCellDep('OMNILOCK'),
-        //     getScriptCellDep('SECP256K1_BLAKE160'),
-        //   ),
-        // );
+        const rawTransaction: Transaction = {
+          ...(tx as unknown as RawTransaction),
+          witnesses: transaction.witnesses.map(() => {
+            const signedWitness = bytes.hexify(
+              blockchain.WitnessArgs.pack({
+                lock: commons.omnilock.OmnilockWitnessLock.pack({
+                  signature: bytes.bytify(signature!).buffer,
+                }),
+              }),
+            );
+            return signedWitness;
+          }),
+        };
 
-        // const witness = bytes.hexify(
-        //   blockchain.WitnessArgs.pack({ lock: OMNILOCK_SIGNATURE_PLACEHOLDER }),
-        // );
-        // inputs.forEach(() => {
-        //   tx = tx.update('witnesses', (witnesses) => witnesses.push(witness));
-        // });
-
-        // tx = commons.omnilock.prepareSigningEntries(tx);
-        // const { message } = tx.signingEntries.get(0)!;
-        // let signature = await signMessage({ message });
-
-        // // Fix ECDSA recoveryId v parameter
-        // // https://bitcoin.stackexchange.com/questions/38351/ecdsa-v-r-s-what-is-v
-        // let v = Number.parseInt(signature.slice(-2), 16);
-        // if (v >= 27) v -= 27;
-        // signature = ('0x' +
-        //   signature.slice(2, -2) +
-        //   v.toString(16).padStart(2, '0')) as typeof signature;
-
-        // const signedWitness = bytes.hexify(
-        //   blockchain.WitnessArgs.pack({
-        //     lock: commons.omnilock.OmnilockWitnessLock.pack({
-        //       signature: bytes.bytify(signature!).buffer,
-        //     }),
-        //   }),
-        // );
-
-        // tx = tx.update('witnesses', (witnesses) =>
-        //   witnesses.set(0, signedWitness),
-        // );
-        // const signedTx = helpers.createTransactionFromSkeleton(tx);
-
-        // return signedTx;
+        console.log(rawTransaction);
+        // const rpc = new RPC('https://testnet.ckbapp.dev/rpc');
+        // const txHash = await rpc.sendTransaction(rawTransaction);
+        // console.log(txHash);
 
         setIsLoading(false);
         return result;
